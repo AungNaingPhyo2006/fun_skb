4c0e9f4e2aba98c4ec23b6db235379f2e9aed0f6
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2024-06-05 21:22:27 +0630

    Modify

--- lib/screens/videos_page/entertainment_page.dart
+++ lib/screens/videos_page/entertainment_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:fun_skb/widgets/productBox.dart';
 
 class EntertainmentPage extends StatelessWidget {
   const EntertainmentPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hi ! I am Entertainment Page'),
+    return ListView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+      children: <Widget>[
+        ProductBox(
+            name: "iPhone",
+            description: "iPhone is the stylist phone ever.",
+            price: 1000,
+            image: "shwe.jpeg"),
+        ProductBox(
+            name: "Pixel",
+            description: "Pixel is the most featureful phone ever",
+            price: 800,
+            image: "shwe.jpeg"),
+        ProductBox(
+            name: "Laptop",
+            description: "Laptop is most productive development tool",
+            price: 2000,
+            image: "shwe.jpeg"),
+        ProductBox(
+            name: "Tablet",
+            description: "Tablet is the most useful device ever for meeting",
+            price: 1500,
+            image: "shwe.jpeg"),
+        ProductBox(
+            name: "Pendrive",
+            description: "Pendrive is useful storage medium",
+            price: 100,
+            image: "shwe.jpeg"),
+        ProductBox(
+            name: "Floppy Drive",
+            description: "Floppy drive is useful rescue storage medium",
+            price: 20,
+            image: "shwe.jpeg"),
+      ],
     );
   }
 }
--- lib/screens/videos_page/knowledge_page.dart
+++ lib/screens/videos_page/knowledge_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:fun_skb/widgets/myTextWidget.dart';
 
 class KnowledgePage extends StatelessWidget {
   const KnowledgePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hi , I am KnowledgePage Page'),
+    return ListView(
+      children: <Widget>[
+        MyTextWidget(
+          name: "iPhone",
+          description: "iPhone is the stylist phone ever.",
+        ),
+        MyTextWidget(
+          name: "iPhone",
+          description: "iPhone is the stylist phone ever.",
+        )
+      ],
     );
   }
 }
